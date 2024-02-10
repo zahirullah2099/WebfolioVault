f5260547f2b264c7a74be00848834f20174789f7
zahir ullah <zahirullah2099@gmail.com>
2024-02-10 23:34:31 +0500

    i have done my portfolio
    i have done my portfolio

---
+++ script.js
@@ -0,0 +1,16 @@
+const header = document.querySelector("header");
+window.addEventListener("scroll",  () =>{
+    header.classList.toggle("sticky" , window.scrollY > 120)
+})
+
+let menu = document.querySelector("#menu-icon")
+let navlist = document.querySelector(".navlist")
+
+menu.onclick = ()=>{
+    menu.classList.toggle("bx-x")
+    navlist.classList.toggle ("active")
+}
+window.scroll = ()=>{
+    menu.classList.remove("bx-x")
+    navlist.classList.remove("active")
+}
